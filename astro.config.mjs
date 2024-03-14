3c3aefb89d61ef1fab54cb47e80af1bc359675ca
Clara Cai <clara.cai@tpisoftware.com>
2024-03-14 21:07:48 +0800

    github actions script

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,6 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://bearkern.github.io/",
-  base: "/",
   integrations: [
     tailwind(),
     swup({
